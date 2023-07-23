9fa06b9c712114f3e0b7df3966ab551371bde3da
Arjun <arjundasback515@gmail.com>
2023-07-23 14:59:19 +0600

    fetch the data on asuseal and create the cart then pass the data dynamic

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductCart from '../ProductCart/ProductCart';
 
 const Home = () => {
+    const [products,setProduct]=useState([])
+
+    useEffect(()=>{
+        fetch("products.json")
+        .then(res=>res.json())
+        .then(data=>setProduct(data))
+    },[])
+
+    console.log(products);
     return (
-        <div>
-            this is home
+        <div className='grid gap-4 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 grid-rows-3 lg:p-32 p-5 md:p-10'>
+            {
+                products.map((product)=> <ProductCart key={product._id} product={product}></ProductCart> )
+            }
         </div>
     );
 };
